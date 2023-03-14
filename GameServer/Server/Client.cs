7fefbe5393a781ec5ea617744c2f7fbc0c1e4f0e
刘涛\dmlt <2985826650@qq.com>
2023-03-14 10:05:39 +0800

    上传服务器端架构

---
+++ GameServer/Server/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Server
+{
+    //这个类是每个客户端具体处理通信的一个类
+    class Client
+    {
+        private Socket ClientSocket;
+        private Server Server;
+        private Message message = new Message();
+        public Client() { }
+        public Client(Socket clientSocket, Server server)
+        {
+            this.ClientSocket = clientSocket;
+            this.Server = server;
+        }
+        public void Start() 
+        {
+            //开始接收
+            ClientSocket.BeginReceive(message.Data, message.StartIndex,message.RemainSize,SocketFlags.None,ReceiveCallBack,null);
+        }
+        private void ReceiveCallBack(IAsyncResult ar) 
+        {
+            try
+            {
+                int count = ClientSocket.EndReceive(ar);
+                if (count == 0)
+                {
+                    Close();
+                }
+                //TODO 处理接收到的数据
+                message.ReadMessage(count);
+
+                Start();
+                ClientSocket.BeginReceive(null,0,0,SocketFlags.None,ReceiveCallBack,null);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                Close();
+            }
+            finally { }
+        }
+        private void Close()
+        {
+            if (ClientSocket != null) 
+            {
+                ClientSocket.Close();
+                this.Server.RemoveClient(this);
+            };
+        }
+    }
+}
---
+++ GameServer/Server/Message.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Server
+{
+    class Message
+    {
+        private byte[] data = new byte[1024];
+        //我们存储了多少个字节的数据在里面
+        private int startIndex = 0;
+        public byte[] Data
+        {
+            get { return data; }
+            private set { }
+        }
+        public int StartIndex
+        {
+            get { return startIndex; }
+            private set { }
+        }
+        public int RemainSize 
+        {
+            get { return data.Length - startIndex; }
+        }
+        public void AddCount(int count) 
+        {
+            startIndex += count;
+        }
+        /// <summary>
+        /// 解析数据
+        /// </summary>
+        public void ReadMessage (int cunt)
+        {
+            AddCount(cunt);
+            while (true) 
+            {
+                if (startIndex <= 4) 
+                { 
+                    return;
+                }
+                int count = BitConverter.ToInt32(data, 0);
+                if (startIndex - 4 > 0)
+                {
+                    string s = Encoding.UTF8.GetString(data, 4, count);
+                    Console.WriteLine("解析出来一条数据：" + s);
+                    Array.Copy(data, count + 4, data, 0, startIndex - count);
+                    startIndex -= count;
+                }
+                else 
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
